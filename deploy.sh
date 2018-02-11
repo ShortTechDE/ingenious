b1e8bc61558edcba903e11818cb096255f9bc420
TheCrealm <me@thecrealm.de>
2018-02-11 22:50:53 +0100

    Update deploy script
    Remove options for StrictHostKey checking.

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 ls
-sshpass -p "$SSH_PASS" rsync -av -e ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no --exclude='/node_modules' * $SSH_USER@$SSH_HOST:/var/www/ghost/content/themes/ingenious
+sshpass -p "$SSH_PASS" rsync -av -e ssh --exclude='/node_modules' * $SSH_USER@$SSH_HOST:/var/www/ghost/content/themes/ingenious
