64dda12ef862b8e9e91cde8874c0a187db7e8863
Flo <x.client.ich@gmail.com>
2018-02-11 21:07:09 +0100

    Deploy v2

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 ls
-sshpass -p "$SSH_PASS" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q -r * $SSH_USER@$SSH_HOST:/var/www/ghost/content/themes
+sshpass -p "$SSH_PASS" scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -q -r * $SSH_USER@$SSH_HOST:/var/www/ghost/content/themes/ingenious
