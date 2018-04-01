84af9a159cbc69f8d49e7cc815d6989d2f0b75f1
Flo <x.client.ich@gmail.com>
2018-04-01 17:17:11 +0200

    :green_heart: Fixed CI Build

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,2 @@
 ls
-sshpass -p "$SSH_PASS" rsync -av -e ssh --exclude='/node_modules' * $SSH_USER@$SSH_HOST:/var/www/ghost/content/themes/ingenious
+sshpass -p "$SSH_PASS" rsync -O -av -e ssh --exclude='/node_modules' * $SSH_USER@$SSH_HOST:/var/www/ghost/content/themes/ingenious
